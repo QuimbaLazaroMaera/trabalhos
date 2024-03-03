fac2216e993144284b0f69770c94d06ac3ac75ed
QuimbaMaera <161809921+QuimbaLazaroMaera@users.noreply.github.com>
2024-03-03 22:39:32 +0200

    Update selection_sort_Recusrivo.java

--- selection_sort_Recusrivo.java
+++ selection_sort_Recusrivo.java
@@ -2,25 +2,30 @@ package Exercicios;
 public class selection_sort_Recusrivo {
     public static void metodo(int [] arr,int tamanho){
         
-        if(tamanho ==1)
+        if(tamanho <=1)
             return;
-        for(int i=0;i<arr.length-1;i++){
-            int eleito = arr[i];
-            int menor = arr[i+1];
-            int posicaoMenor = i+1;
-         if(arr[i+1]<eleito){
-            menor = arr[i+1];
-            posicaoMenor = i+1;
+         metodo(arr,tamanho-1);
+          metodo2(arr,tamanho-1);
+    }
+    
+    public static void metodo2(int [] arr,int tamanho){
+        if(tamanho<=0) return ;
+             int i = tamanho;
+            int eleito = arr[i-1];
+            int menor = arr[i];
+            int posicaoMenor = i;
+         if(arr[i-1]<eleito ){
+            menor = arr[i-1];
+            posicaoMenor = i-1;
          } 
         
            if(menor<eleito){
-               arr[i] = menor;
+               arr[i-1] = menor;
                arr[posicaoMenor] = eleito;
            }
-        }
-         metodo(arr,tamanho-1);
-    }
-    
+            metodo2(arr,tamanho-1);
+          
+    }   
     public static void main(String[] args) {
         int a[] = {9,4,6,2,5};
         
