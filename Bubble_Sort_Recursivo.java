4a6158a442b4a4821bc2dd63ca7974c82c391e13
QuimbaMaera <161809921+QuimbaLazaroMaera@users.noreply.github.com>
2024-03-03 23:00:23 +0200

    Update Bubble_Sort_Recursivo.java

--- Bubble_Sort_Recursivo.java
+++ Bubble_Sort_Recursivo.java
@@ -3,18 +3,22 @@ package Exercicios;
 public class Bubble_Sort_Recursivo {
     private static void metodo(int [] arr,int tamanho){
         
-        if(tamanho==1) 
+        if(tamanho<=1) 
             return;
-        for(int i=0;i<tamanho-1;i++){
-        if(arr[i]>arr[i+1]){
+        metodo(arr,tamanho-1)
+    }
+
+private static void metodo2(int [] arr,int tamanho){
+    if (tamanho<=0) return;
+       int i = tamanho;
+        if(arr[i]<arr[i-1]){
             int aux = arr[i];
-            arr[i]=arr[i+1];
-            arr[i+1] = aux;
+            arr[i]=arr[i-1];
+            arr[i-1] = aux;
         }
-    }
-         metodo(arr,tamanho-1);
-        
-    }
+    
+         metodo2(arr,tamanho-1);    
+}
     
   public static void main(String[] args) {
         int [] a = {8,2,4,9,10};
